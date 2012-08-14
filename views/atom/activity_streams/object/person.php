3b7911008e7d1dbd21b3f38c4115a9a77ca40449
Pablo Martin <caedes@sindominio.net>
2012-08-14 11:40:13 +0000

    fix usage of not declared $user variable.

--- views/atom/activity_streams/object/person.php
+++ views/atom/activity_streams/object/person.php
@@ -8,6 +8,6 @@ $person = $vars['entity'];
 <name><?php echo elgg_view('output/text', array('value' => $person->name)); ?></name>
 <summary><?php echo elgg_view('output/text', array('value' => $person->briefdescription)); ?></summary>
 <link href="<?php echo $person->getIcon('small'); ?>" rel="preview" type="image/png" media:height="40" media:width="40" />
-<link href="<?php echo $user->getIcon('small'); ?>" rel="avatar" type="image/png" media:height="40" media:width="40" />
+<link href="<?php echo $person->getIcon('small'); ?>" rel="avatar" type="image/png" media:height="40" media:width="40" />
 <link href="<?php echo $person->getURL(); ?>" rel="alternate" type="text/html" />
 <activity:object-type>person</activity:object-type>
