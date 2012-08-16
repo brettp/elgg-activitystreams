af91422e6d5f172ffcefddab7cc3f59268310de8
Brett Profitt <brett.profitt@gmail.com>
2012-08-16 17:38:05 -0400

    Added ability to override views for river items and fixed comments.

--- views/stream_json/annotation/generic_comment.php
+++ views/stream_json/annotation/generic_comment.php
@@ -1,3 +1,10 @@
 <?php
 
+$annotation = elgg_extract('annotation', $vars);
+$entity = $annotation->getEntity();
+
+$vars['author'] = elgg_view_entity($annotation->getOwnerEntity());
+$vars['in_reply_to'] = elgg_view_entity($entity);
+$vars['content'] = $annotation->value;
+
 echo elgg_view('activity_streams/object/comment', $vars);
