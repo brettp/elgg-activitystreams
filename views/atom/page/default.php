db2b703eb6b8a35ae3ba5c6865515b9485c27290
Pablo Martin <caedes@sindominio.net>
2012-10-02 08:59:12 +0000

    add activity streams context xml ns

--- views/atom/page/default.php
+++ views/atom/page/default.php
@@ -28,7 +28,7 @@ $url = elgg_http_add_url_query_elements($full_url, array('view' => 'atom'));
 
 echo "<?xml version='1.0' encoding='UTF-8' ?>\n";
 ?>
-<feed xmlns="http://www.w3.org/2005/Atom" xmlns:thr="http://purl.org/syndication/thread/1.0" xmlns:georss="http://www.georss.org/georss" xmlns:activity="http://activitystrea.ms/spec/1.0/" xmlns:media="http://purl.org/syndication/atommedia" xmlns:poco="http://portablecontacts.net/spec/1.0" xml:lang="en-US" <?php echo elgg_view('extensions/xmlns'); ?>>
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:thr="http://purl.org/syndication/thread/1.0" xmlns:georss="http://www.georss.org/georss" xmlns:activity="http://activitystrea.ms/spec/1.0/" xmlns:media="http://purl.org/syndication/atommedia" xmlns:poco="http://portablecontacts.net/spec/1.0" xmlns:context="http://activitystrea.ms/context/" xml:lang="en-US" <?php echo elgg_view('extensions/xmlns'); ?>>
 	<title><?php echo elgg_view('output/text', array('value' => $title)); ?></title>
 	<id><?php echo $full_url; ?></id>
 	<updated><?php echo date(DATE_ATOM); ?></updated>
